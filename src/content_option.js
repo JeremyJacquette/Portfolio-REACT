06d942d3ee6b76edb6672ee6d2e6cd1f31deb8e8
JeremyJacquette <jer123@gmail.com>
2023-09-13 11:55:27 +0200

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,144 @@
+const logotext = "{Jérémy Jacquette}";
+const meta = {
+    title: "{Jérémy-Portfolio}",
+    description: "Jérémy Jacquette, développeur web et web mobile",
+};
+
+const introdata = {
+    title: "Bonjour explorateur !",
+    animated: {
+        first: "Développeur web",
+        second: "Back-End",
+        third: "Spécialisé en :",
+        fourth:"PHP symfony",
+        // add more if you'd like but make sure to update /src/pages/home/index.js Line 29
+    },
+    description: "Bienvenue sur ma page personnelle. Ici, vous découvrirez mes projets, mes coordonnées professionnelles, ainsi qu'une section approfondie me concernant. ",
+    your_img_url: "https://i.ibb.co/XLSNH3q/10259340-4401280-removebg.png",
+};
+
+const dataabout = {
+    title: "Mon parcours :",
+    aboutme: "Je suis un développeur Web et Web mobile, spécialisé en Back-End avec une expertise particulière dans la technologie Symfony. Mon parcours professionnel est considéré comme atypique, car j'ai effectué une reconversion grâce à l'école O'clock. Mon expérience précédente en tant que sous-chef dans le domaine de la restauration semi-gastronomique constitue une compétence que j'utilise pleinement dans mon travail quotidien.Aujourd'hui, après d'incessants efforts, je suis en mesure de réussir vos projets. Ma curiosité naturelle m'incite à ne pas me limiter au Back-End ; je continue de me former dans divers domaines tels que le Front-End, l'administration système, la cybersécurité, et bien d'autres encore.",
+};
+const worktimeline = [{
+        jobtitle: "RNCP niveau 6 (bac+3/4) 'CDA'",
+        where: "O'clock",
+        date: "(en recherche)",
+    },
+    {
+        jobtitle: "RNCP niveau 5 (bac+2) 'DWWM'",
+        where: "O'clock",
+        date: "2023",
+    },
+    {
+        jobtitle: "CAP Hôtellerie-restauration",
+        where: "Groupe FIM Saint-lô",
+        date: "2017",
+    },
+];
+
+const skills = [
+    {
+        name: "PHP",
+    },
+    
+    {
+        name: "Symfony",
+    },
+    {
+        name: "JavaScript",
+    },
+    {
+        name: "MySQL",
+    },
+    
+    {
+        name: "HTML/CSS",
+    },
+    
+    {
+        name: "Bootstrap",
+    },
+    {
+        name: "Tailwind",
+    },
+
+    {
+        name: "WordPress",
+    },
+
+    {
+        name: "API Rest",
+    },
+
+    {
+        name: "AGILES",
+    },
+
+    {
+        name: "Tests unitaires",
+    },
+    {
+        name: "Powershell",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Projet d'apothéose O'clock.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "Projet de Porfolio de photographe.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "à venir",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "à venir",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "à venir",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "à venir",
+        link: "#",
+    },
+   
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "jeremy.jqt.dev@gmail.com",
+    description: "J'ai hâte de vous répondre !",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_j4d3hcb",
+    YOUR_TEMPLATE_ID: "template_58h82ub",
+    YOUR_USER_ID: "HQqaKUb-nwgjTabiB",
+};
+
+const socialprofils = {
+    github: "https://github.com/JeremyJacquette",
+    linkedin: "https://www.linkedin.com/in/j%C3%A9r%C3%A9my-jacquette-%F0%9F%92%BB-49b9a527a/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
